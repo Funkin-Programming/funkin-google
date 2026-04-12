fb40ae9b3c29f9929bf1d94e31200a930d577e14
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-04-12 13:23:55 -0300

    Implement System class with login and initialization
    Add initialization and login/logout functionality to System class.

--- source/frontend/System.hx
+++ source/frontend/System.hx
@@ -1,7 +1,48 @@
 package frontend;
 
 import backend.BackendManager:
-import backend.BackendConfig;
+import flixel.FlxG;
 
-class System extends {
-}
+class System
+{
+  public static var initialized:Bool = false;
+  public static var loggedIn:Bool = false;
+
+  static var autoSync:Bool = true;
+  static var syncTimer:Float = 0;
+  static var syncInterval:Float = 10;
+
+  public static function init()
+  {
+    if (initialized) return;
+
+    BackendManager.init();
+
+    initialized = true;
+
+    autoLogin();
+  }
+
+  public static function autoLogin()
+  {
+    var auto = backend.BackendConfig.get("firebase.autoLogin");
+
+    if (auto)
+    {
+      login();
+    }
+  }
+
+  public static function login()
+  {
+    BackendManager.google.logout();
+    BackendManager.firebase.login();
+
+    loggedIn = true;
+  }
+
+  public static function logout()
+  {
+    BackendManager.google.logout();
+    loggedIn = false;
+  }
